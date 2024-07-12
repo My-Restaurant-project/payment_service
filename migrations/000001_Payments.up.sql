c4fc42e4ae9a97a1f07c7d4b7abcdf4061d5513a
Fedy1507 <gayratovfirdavs720@gmail.com>
2024-07-12 05:58:24 +0500

    add payment servic

--- migrations/000001_Payments.up.sql
+++ migrations/000001_Payments.up.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS Payments( 
-    id CHAR(36) NOT NULL,
-    reservation_id CHAR(36) NOT NULL,
+    id UUID NOT NULL,
+    reservation_id UUID NOT NULL,
     amount DECIMAL(8, 2) NOT NULL,
     payment_method VARCHAR(255) NOT NULL,
     payment_status VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL,
     PRIMARY KEY(id)
 );
